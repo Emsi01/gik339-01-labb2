640aa3b6e85816df2f09b664012bd945c16e0d4a
Emsi01 <h22emmru@du.se>
2023-12-14 11:37:06 +0100

    uppgift 3

--- server/server.js
+++ server/server.js
@@ -12,4 +12,13 @@ res.header('Access-Control-Allow-Methods', '*');
 next();
 });
 
-server.listen(3000)
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
+
+const sqlite3 = require("sqlite3").verbose()
+
+server.get("/users", (req, res) => {
+    const db = new sqlite3.Database("./gik339-labb2.db");
+
+});
