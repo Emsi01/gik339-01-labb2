23393cb1d6234313ec1983e9d64a134a6f69d887
Emsi01 <h22emmru@du.se>
2023-12-15 12:06:33 +0100

    klar

--- client/script.js
+++ client/script.js
@@ -1,12 +1,25 @@
 
 const url = 'http://localhost:3000';
 
-document.createElement(ul)
-
 fetch(url + '/users')
     .then(res => res.json())
     .then(data => {
       console.log(data)
-      data.forEach(user => {
+
+    const user_ul = document.createElement("ul");
+    data.forEach(user => {
+        const user_li = document.createElement('li');
+        user_li.innerHTML = `<p> ${user.firstName} ${user.lastName} <br> ${user.username} </p>`;
+        user_ul.appendChild(user_li);
+
+        user_li.style.backgroundImage = `linear-gradient(to right, ${user.color}, white)`;
+        user_li.style.padding = "1px 1px 1px 15px";
+        user_li.style.border = "solid black 2px";
+        user_li.style.marginBottom = "3px";
+       
       })
-});
+      
+    document.body.appendChild(user_ul);}
+);
+
+
